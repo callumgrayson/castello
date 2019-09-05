86e51f7942a16c843ef0b266c15f4476e148c821
callumgrayson <callum@musiio.com>
2019-09-05 16:58:37 +0800

    Updates

---
+++ precache-manifest.9be31728082295d9336bd67b6bc780b2.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ecfdb233b68b3ad089a7cfcf61496d4",
+    "url": "/castello/index.html"
+  },
+  {
+    "revision": "2603a415787adf8b97f7",
+    "url": "/castello/static/css/main.f66d8358.chunk.css"
+  },
+  {
+    "revision": "a6bc25d656c5f28edb14",
+    "url": "/castello/static/js/2.a41f6bb4.chunk.js"
+  },
+  {
+    "revision": "2603a415787adf8b97f7",
+    "url": "/castello/static/js/main.6b4a86ba.chunk.js"
+  },
+  {
+    "revision": "4ef1b02ba79bea30aa29",
+    "url": "/castello/static/js/runtime~main.b20247e2.js"
+  },
+  {
+    "revision": "f250e12628a7b2a12cb9141fa2e5cda1",
+    "url": "/castello/static/media/back.f250e126.jpg"
+  },
+  {
+    "revision": "60a963d82b5bd5b32df795062a348a3b",
+    "url": "/castello/static/media/favicon.60a963d8.png"
+  },
+  {
+    "revision": "581fc6ad45a1428157168bfff7c40e03",
+    "url": "/castello/static/media/gal-1.581fc6ad.jpeg"
+  },
+  {
+    "revision": "9865e700f1cd5e17135a6f75da6b1378",
+    "url": "/castello/static/media/gal-10.9865e700.jpeg"
+  },
+  {
+    "revision": "5536a90f3836ebf8085ec0ae46984867",
+    "url": "/castello/static/media/gal-11.5536a90f.jpeg"
+  },
+  {
+    "revision": "0709a3b0434b69b42cdd096e4d13f90f",
+    "url": "/castello/static/media/gal-12.0709a3b0.jpeg"
+  },
+  {
+    "revision": "b4740c9c0d2cec5640904289867c8ce6",
+    "url": "/castello/static/media/gal-13.b4740c9c.jpeg"
+  },
+  {
+    "revision": "486c8880b61fee96a95de85288a1e6ae",
+    "url": "/castello/static/media/gal-14.486c8880.jpeg"
+  },
+  {
+    "revision": "ee7b3ca7ee80e0b5c62391c6ec93a671",
+    "url": "/castello/static/media/gal-2.ee7b3ca7.jpeg"
+  },
+  {
+    "revision": "628f87aeedf507d437f6aeb12bcc4215",
+    "url": "/castello/static/media/gal-3.628f87ae.jpeg"
+  },
+  {
+    "revision": "c6fff3a13ac92abd83a80c208c4dda53",
+    "url": "/castello/static/media/gal-4.c6fff3a1.jpeg"
+  },
+  {
+    "revision": "4114ded407c6764a037d322452fd6ca0",
+    "url": "/castello/static/media/gal-5.4114ded4.jpeg"
+  },
+  {
+    "revision": "aa2376b5a04a3bf548d9f1773f580da1",
+    "url": "/castello/static/media/gal-6.aa2376b5.jpeg"
+  },
+  {
+    "revision": "6085a127abe2dab815ab22ec0a9135d1",
+    "url": "/castello/static/media/gal-7.6085a127.jpeg"
+  },
+  {
+    "revision": "f30fbda69d5eca2f340eb6d4d093a914",
+    "url": "/castello/static/media/gal-8.f30fbda6.jpeg"
+  },
+  {
+    "revision": "92758fee44f610a3b225c33ff90a9333",
+    "url": "/castello/static/media/gal-9.92758fee.jpeg"
+  },
+  {
+    "revision": "d11b666fa6c694c620ff867c6669cf7f",
+    "url": "/castello/static/media/hero.d11b666f.jpeg"
+  },
+  {
+    "revision": "5120ad222b9080b343187d3c51e851f3",
+    "url": "/castello/static/media/house_1.5120ad22.jpeg"
+  },
+  {
+    "revision": "ed12ff11bf26663331ad01ff18d3ebfb",
+    "url": "/castello/static/media/house_2.ed12ff11.jpeg"
+  },
+  {
+    "revision": "521c065d9e8bc6ff2ec850a3ea58a4bf",
+    "url": "/castello/static/media/house_3.521c065d.jpeg"
+  },
+  {
+    "revision": "83ee1d1e0e83198aa12b8a63af3f8a2b",
+    "url": "/castello/static/media/house_4.83ee1d1e.jpeg"
+  },
+  {
+    "revision": "721effb4a9ef7f3ac5e281cb13cda239",
+    "url": "/castello/static/media/house_5.721effb4.jpeg"
+  },
+  {
+    "revision": "ee021f8ac6ebfadeab35a43e6ef6b306",
+    "url": "/castello/static/media/house_6.ee021f8a.jpeg"
+  },
+  {
+    "revision": "ee16c7b8a84ac078afc2b46923cc6187",
+    "url": "/castello/static/media/logo-bi.ee16c7b8.png"
+  },
+  {
+    "revision": "e5c174a02df208c513cfb932428e84c9",
+    "url": "/castello/static/media/logo-forbes.e5c174a0.png"
+  },
+  {
+    "revision": "a473e3878ca044d42ba86f99b685ba47",
+    "url": "/castello/static/media/logo-techcrunch.a473e387.png"
+  },
+  {
+    "revision": "406400ead91831b741f5246c50146142",
+    "url": "/castello/static/media/realtor-1.406400ea.jpeg"
+  },
+  {
+    "revision": "c11e84dcdad4dbf1cb49ff5813ed6611",
+    "url": "/castello/static/media/realtor-2.c11e84dc.jpeg"
+  },
+  {
+    "revision": "af9ff289ef4aa6e35a09b66ec1e42419",
+    "url": "/castello/static/media/realtor-3.af9ff289.jpeg"
+  },
+  {
+    "revision": "a9dfe6a22ad9375f588d45e940bae5d3",
+    "url": "/castello/static/media/sprite.a9dfe6a2.svg"
+  },
+  {
+    "revision": "24268b24588bddc750dc3b45bd820459",
+    "url": "/castello/static/media/story-1.24268b24.jpeg"
+  },
+  {
+    "revision": "915f2bd1ebf80f9d775aa9954f294269",
+    "url": "/castello/static/media/story-2.915f2bd1.jpeg"
+  }
+]);
